2c64bb17df86372c9033528ac608f9866b22c281
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-05-17 17:11:14 +0530

    added new dp solutions

---
+++ Dynamic Programming/312. Burst Balloons (leetcode).cpp
@@ -0,0 +1,54 @@
+/*
+*
+*Problem Link : https://leetcode.com/problems/burst-balloons/
+*Platform: leetcode
+*Status: correct
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false)
+#define boost cin.tie(NULL)
+#define booster cout.tie(NULL)
+#define endl "\n"
+typedef long long int lld;
+
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+using namespace std;
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        
+        vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), 0));
+        
+        for (int len = 1; len <= n; ++len)
+            
+            for (int left = 1; left <= n - len + 1; ++left) {
+                
+                int right = left + len - 1;
+                
+                for (int k = left; k <= right; ++k)
+                    
+                    dp[left][right] = max(dp[left][right], nums[left-1]*nums[k]*nums[right+1] + dp[left][k-1] + dp[k+1][right]);
+            }
+        
+        return dp[1][n];
+    }
+};
+
+
