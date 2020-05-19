eed8df97fd8df771d264a3308205fc52a2e46ec6
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-05-19 17:18:23 +0530

    adding new greed solutions

---
+++ Greedy/991. Broken Calculator (leetcode).cpp
@@ -0,0 +1,43 @@
+/*
+*
+*Problem Link : https://leetcode.com/problems/broken-calculator/
+*Platform: leetcode
+*Status: correct
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false)
+#define boost cin.tie(NULL)
+#define booster cout.tie(NULL)
+#define endl "\n"
+typedef long long int lld;
+
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+using namespace std;
+
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int count = 0;
+        
+        while (Y > X) {
+            
+            Y = Y % 2 > 0 ? Y + 1 : Y / 2;
+            count++;
+        }
+        
+        return count + X - Y;
+    }
+};
+
+
