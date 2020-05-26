f829f8c442491550e53c42db0280ec4fe2be7feb
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-05-27 00:04:31 +0530

    adding new bitmasking solution

---
+++ Bitmasking/397. Integer Replacement (leetcode).cpp
@@ -0,0 +1,52 @@
+/*
+*
+*Problem Link : https://leetcode.com/problems/integer-replacement/
+*Platform: leetcode
+*Status: correct
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false)
+#define boost cin.tie(NULL)
+#define booster cout.tie(NULL)
+#define endl "\n"
+typedef long long int lld;
+
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+using namespace std;
+
+class Solution {
+public:
+    int integerReplacement(int n) {
+        return countRec(n,0);
+    }
+private:
+    int countRec(int n,int count){
+        if(n==1)
+            return count;
+        if(n==3)
+            return count+2;
+        if((n&1)==0){
+            return countRec((n>>1),count+1);
+        }
+        long long a=(long long)n+1;
+        if((a&3)==0){
+            a=a>>1;
+            return countRec((int)a,count+2);
+        }
+        return countRec(n-1,count+1);
+    }
+};
+
+
+
