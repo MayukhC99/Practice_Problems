f7f91c84b3ec0dec8b96564f0fd9d0b521ec2fe4
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-05-26 16:32:38 +0530

    adding new bitmasking solution

---
+++ Bitmasking/191. Number of 1 Bits (leetcode).cpp
@@ -0,0 +1,50 @@
+/*
+*
+*Problem Link : https://leetcode.com/problems/number-of-1-bits/
+*Platform: leetcode
+*Status: correct
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false)
+#define boost cin.tie(NULL)
+#define booster cout.tie(NULL)
+#define endl "\n"
+typedef long long int lld;
+
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        
+        //doing it without using the stl function
+        int count=0;
+        while(n){
+            int last= n & (-n);
+            
+            if(last)
+                count++;
+            n= n - last;
+        }
+        
+        return count;
+        
+        //we can do this using stl function too :-
+        //return __builtin_popcount(n);
+    }
+};
+
+
+
