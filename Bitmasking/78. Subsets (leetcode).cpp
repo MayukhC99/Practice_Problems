03abee0d09f2c17ee73440c8bd86cb30699e8590
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-05-26 20:28:54 +0530

    adding new bitmasking solution

---
+++ Bitmasking/78. Subsets (leetcode).cpp
@@ -0,0 +1,58 @@
+/*
+*
+*Problem Link : https://leetcode.com/problems/subsets/submissions/
+*Platform: leetcode
+*Status: correct
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false)
+#define boost cin.tie(NULL)
+#define booster cout.tie(NULL)
+#define endl "\n"
+typedef long long int lld;
+
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+        
+        int n= nums.size();
+        for(int i = 0 ; i < (1<<n) ; i++ ){
+            
+            int dig = i;
+            vector<int> sub_ans;
+            
+            while(dig){
+                int s= dig & (-dig);
+                
+                int pos= log2(s);
+                sub_ans.push_back(nums[pos]);
+                
+                dig= dig - s;
+            }
+            
+            ans.push_back(sub_ans);
+        }
+        
+        
+        return ans;
+    }
+};
+
+
+
