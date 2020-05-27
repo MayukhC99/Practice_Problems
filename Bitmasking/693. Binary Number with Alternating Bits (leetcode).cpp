2c05bda27add307b0d9eec312ac71cbf549ab9af
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-05-27 12:17:34 +0530

    adding new bitmasking solution

---
+++ Bitmasking/693. Binary Number with Alternating Bits (leetcode).cpp
@@ -0,0 +1,57 @@
+/*
+*
+*Problem Link : https://leetcode.com/problems/binary-number-with-alternating-bits/
+*Platform: leetcode
+*Status: correct
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false)
+#define boost cin.tie(NULL)
+#define booster cout.tie(NULL)
+#define endl "\n"
+typedef long long int lld;
+
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+using namespace std;
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        
+        if(n==0)
+            return false;
+        
+        int flag = 0; 
+        int last = n & 1;
+        if(last)
+            flag= 1;
+        
+        while(n){
+            int l= n & 1;
+    
+            if(l != flag)
+                return false;
+            
+            n>>= 1;
+            flag= !flag;
+        }
+        
+        
+        return true;
+        
+    }
+};
+
+
+
