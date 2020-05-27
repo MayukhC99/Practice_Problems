f5bb50cec412d5f3e5fce0e0926130cf5e33a69a
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-05-27 22:20:25 +0530

    adding new bitmasking solution

---
+++ Bitmasking/477. Total Hamming Distance (leetcode).cpp
@@ -0,0 +1,54 @@
+/*
+*
+*Problem Link : https://leetcode.com/problems/total-hamming-distance/
+*Platform: leetcode
+*Status: correct
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false)
+#define boost cin.tie(NULL)
+#define booster cout.tie(NULL)
+#define endl "\n"
+typedef long long int lld;
+
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+using namespace std;
+
+
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int res = 0;
+        int N = nums.size();
+        
+        for (int bit = 0; bit < 32; bit++) {
+            
+            int bits_1 = 0;
+            for (int num : nums) {
+                
+                if ((num >> bit) & 1 == 1)
+                    bits_1++;
+            }
+            
+            res += bits_1 * (N - bits_1);
+        }
+        
+        
+        
+        return res;
+    }
+};
+
+
+
