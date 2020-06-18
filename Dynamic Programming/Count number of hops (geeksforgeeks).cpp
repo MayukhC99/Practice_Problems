f3f92128f2399659a1278b84c6b1d2c5c741243f
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-06-18 13:47:12 +0530

    ading dp solutions

---
+++ Dynamic Programming/Count number of hops (geeksforgeeks).cpp
@@ -0,0 +1,61 @@
+/*
+*
+*Problem Link : https://practice.geeksforgeeks.org/problems/count-number-of-hops/0
+*Platform: geeksforgeeks
+*Status: correct
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define booster cout.tie(NULL)
+#define endl "\n"
+
+typedef long long int lld;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define si second
+#define pii pair<lld,lld>
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+#define mod 1000000007
+const lld inf= 1e12;
+
+using namespace std;
+
+
+int main() {
+	int dp[51] = {0};
+	dp[0] = 1;
+	dp[1] = dp[0];
+	dp[2] = dp[1] + dp[0];
+	dp[3] = dp[2] + dp[1] + dp[0];
+	for(int i = 4; i<=50 ; i++){
+	    dp[i]+= dp[i-1];
+	    dp[i]+= dp[i-2];
+	    dp[i]+= dp[i-3];
+	}
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int N;
+	    cin >> N;
+	    cout << dp[N] << endl;
+	}
+	
+	return 0;
+}
+
+
