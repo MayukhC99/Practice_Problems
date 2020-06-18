6f66aa13001b9891c8666a24c3240a3685e613e2
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-06-18 23:11:23 +0530

    ading dp solutions

---
+++ Dynamic Programming/Unique BST's (geeksforgeeks).cpp
@@ -0,0 +1,60 @@
+/*
+*
+*Problem Link :
+*Platform:
+*Status:
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define booster cout.tie(NULL)
+#define endl "\n"
+
+typedef long long int lld;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define si second
+#define pii pair<lld,lld>
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+#define mod 1000000007
+const lld inf= 1e12;
+
+using namespace std;
+
+lld dp[20] = {0} ;
+lld catalan(int n){ 
+    dp[0] = dp[1] = 1; 
+    for (int i=2; i<=n; i++) { 
+        dp[i] = 0; 
+        for (int j=0; j<i; j++) 
+            dp[i] += dp[j] * dp[i-j-1]; 
+    } 
+    return dp[n]; 
+} 
+
+
+int main(){
+	speed;
+	int T;
+	cin >> T;
+	while(T--){
+		int N;
+		cin >> N;
+		cout << catalan(N) << endl;
+	}
+	return 0;
+}
+
+
