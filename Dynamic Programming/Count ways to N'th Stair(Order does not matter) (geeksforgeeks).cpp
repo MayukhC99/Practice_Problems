62fb773b95ca197941b0ec655d6c578a5db65bdc
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-06-18 20:28:16 +0530

    ading dp solutions

---
+++ Dynamic Programming/Count ways to N'th Stair(Order does not matter) (geeksforgeeks).cpp
@@ -0,0 +1,68 @@
+/*
+*
+*Problem Link :
+*Platform:
+*Status:
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define booster cout.tie(NULL)
+#define endl "\n"
+
+typedef long long int lld;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define si second
+#define pii pair<lld,lld>
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+#define mod 1000000007
+const lld inf= 1e12;
+
+using namespace std;
+
+long long countWays(int);
+
+int main(){
+	speed;
+	
+	int t;
+	cin >> t;
+	while(t--){
+		int m;
+		cin >> m;
+		cout << countWays(m) << endl;
+	}
+	
+	return 0;
+}
+
+
+// function to count ways to reach mth stair
+long long dp[1000003]={0};
+long long countWays(int m){
+    if(m < 0)
+        return 0;
+    if(m == 0)
+        return 1;
+    if(dp[m] != 0)
+        return dp[m];
+    
+    dp[m] = 1 + countWays(m-2) ;
+    
+    return dp[m];
+}
+
+
