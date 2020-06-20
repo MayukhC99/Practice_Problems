bef159f4b3aaa9e7b48f909856ccc34353e062fd
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-06-20 23:01:53 +0530

    adding dp solution

--- Dynamic Programming/Unique BST's (geeksforgeeks).cpp
+++ Dynamic Programming/Unique BST's (geeksforgeeks).cpp
@@ -36,7 +36,7 @@ using namespace std;
 lld dp[20] = {0} ;
 lld catalan(int n){ 
     dp[0] = dp[1] = 1; 
-    for (int i=2; i<=n; i++) { 
+    for (int i=2; i<=n; i++){ 
         dp[i] = 0; 
         for (int j=0; j<i; j++) 
             dp[i] += dp[j] * dp[i-j-1]; 
