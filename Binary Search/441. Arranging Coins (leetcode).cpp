910127b189f5b7bbaf12f4279dfe4f1e88597469
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-08-03 21:11:29 +0530

    adding Binary Search problem

---
+++ Binary Search/441. Arranging Coins (leetcode).cpp
@@ -0,0 +1,54 @@
+/*
+*
+*Problem Link : https://leetcode.com/problems/arranging-coins/
+*Platform: 441. Arranging Coins
+*Status: correct
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define booster cout.tie(NULL)
+#define endl "\n"
+
+typedef long long int lld;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define si second
+#define pii pair<lld,lld>
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+#define mod 1000000007
+const lld inf= 1e12;
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long l=0,r=n;
+        while(l<=r){
+            long long mid = l + (r-l)/2;
+            long long check = (mid*(mid+1))/2;
+            
+            if(check > n)
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return r;
+    }
+};
+
