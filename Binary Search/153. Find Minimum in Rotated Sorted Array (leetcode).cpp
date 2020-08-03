98f40a0dd86c80fcce95c2d55175ac9572f4ec19
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-08-03 21:13:33 +0530

    adding Binary Search problem

---
+++ Binary Search/153. Find Minimum in Rotated Sorted Array (leetcode).cpp
@@ -0,0 +1,61 @@
+/*
+*
+*Problem Link : https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+*Platform: leetcode
+*Status: correct
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define booster cout.tie(NULL)
+#define endl "\n"
+
+typedef long long int lld;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define si second
+#define pii pair<lld,lld>
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+#define mod 1000000007
+const lld inf= 1e12;
+
+using namespace std;
+
+
+class Solution {
+public:
+    
+    int binary(vector<int>& ar,int l,int r){
+ 
+        while(l<r){
+            if(ar[l]<ar[r])
+                return ar[l];
+            
+            int mid = l + (r-l)/2 ;
+            
+            if(ar[mid]>=ar[l])
+                l = mid+1;
+            else
+                r = mid;
+        }
+        return ar[l];
+    }
+    
+    int findMin(vector<int>& nums) {
+        return binary(nums , 0 , nums.size()-1);
+    }
+};
+
+
