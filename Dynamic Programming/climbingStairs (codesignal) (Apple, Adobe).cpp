89c95e48bc3533aabbe95feeedafa703ffcef647
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-08-31 09:07:46 +0530

    adding dp solution

---
+++ Dynamic Programming/climbingStairs (codesignal) (Apple, Adobe).cpp
@@ -0,0 +1,48 @@
+/*
+*
+*Problem Link : https://app.codesignal.com/interview-practice/task/oJXTWuwEZiC6FTw3A
+*Platform: codesignal
+*Status: correct
+*Author: Mayukh Chakrabarti
+*
+*/
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define booster cout.tie(NULL)
+#define endl "\n"
+
+typedef long long int lld;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define si second
+#define pii pair<lld,lld>
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+
+#define mod 1000000007
+const lld inf= 1e12;
+
+using namespace std;
+
+int dp[100] = {0};
+
+int climbingStairs(int n) {
+    if(n == 0)
+        return 1;
+    if(n < 0)
+        return 0;
+    if(dp[n] != 0)
+        return dp[n];
+    return dp[n] = climbingStairs(n-1) + climbingStairs(n-2);
+}
+
+
